d0c37389be24a915cce111909211578b565248fd
dvsdevious <daria.sheremeteva@gmail.com>
2021-10-24 22:49:27 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import Weather from "./Weather"
 
 function App() {
   return (
-   <Weather/>)
+   <div className="App">
+     <Weather/>
+     <footer>
+       This project was coded by Daria S. and is open-sourced {""}
+     <a href="https://github.com/dvsdevious/weather-app-react.git" target="_blank"> on Github.
+     </a>
+     </footer>
+     </div>)
 }
 
 export default App;
