587e320164b64d6bc7f9862348d6b6db2a0e9035
dvsdevious <daria.sheremeteva@gmail.com>
2021-11-07 21:54:39 +0300

    created the 5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,40 @@
-import React from "react"
-import WeatherIcon from "./WeatherIcon"
+import React, {useState} from "react"
 import "./WeatherForecast.css"
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+let [loaded, setLoaded] = useState(false)
+let [forecast, setForecast] = useState(null)
+
+function handleResponse(response) {
+setForecast(response.data.daily)
+setLoaded(true);
+}
+
+if (loaded) {
     return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col"> 
-                <div className="forecast-day">Thu</div>
-                <WeatherIcon code="01d" size="32"/>
-                <div className="WeatherForecast-temperatures">
-                    <span className="WeatherForecast-temperature-max">19º</span>
-                    <span className="WeatherForecast-temperature-min">10º</span>
-                </div>
-                </div>
-                </div> 
-            </div>
-    )
+    <div className="WeatherForecast">
+        <div className="row">
+            {forecast.map(function(dailyForecast, index) {
+                if (index<5) {
+                return (
+                <div className="col" key={index}> 
+                <WeatherForecastDay data={dailyForecast}/>
+                 </div>
+                )
+                }
+            })}
+        </div> 
+    </div>);
+} else {
+    let apiKey="d0480346da20b22ca13a5aaa0631d64b"
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+    }
 }
